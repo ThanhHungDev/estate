57f23527141c52f6ce7f490c1f73278f79bbc76c
Trương Thanh hùng <thuong_thanh_hung@managerment-partners.co.jp>
2020-11-27 11:39:00 +0900

    updsate

--- SERVER_PHP/resources/views/client/layout/index.blade.php
+++ SERVER_PHP/resources/views/client/layout/index.blade.php
@@ -20,11 +20,11 @@
     <link rel="preload" href="/font/font-awe/webfonts/fa-light-300.woff2" as="font" />
     <link rel="preload" href="/font/font-awe/webfonts/fa-regular-400.woff2" as="font" />
     <link rel="preload" href="/font/font-awe/webfonts/fa-solid-900.woff2" as="font" /> --}}
-    <link async rel="preload" as="style" href="{{ asset('css/awesome.min.css' . Config::get('app.version'))}}">
-    <link async rel="preload" as="style" href="{{ asset('css/client.min.css' . Config::get('app.version'))}}">
-    <script async rel="preload" as="script" src='{{ asset('js/library/jquery.min.js' . Config::get('app.version')) }}'></script>
-    <script async rel="preload" as="script" src="{{ asset('js/library/tipped.min.js' . Config::get('app.version')) }}"></script>
-    <script async rel="preload" as="script" src="{{ asset('js/client/app.min.js' . Config::get('app.version')) }}"></script>
+    <link rel="preload" as="style" href="{{ asset('css/awesome.min.css' . Config::get('app.version'))}}">
+    <link rel="preload" as="style" href="{{ asset('css/client.min.css' . Config::get('app.version'))}}">
+    
+    {{-- <script rel="preload" as="script" src='{{ asset('js/library/jquery.min.js' . Config::get('app.version')) }}'></script> --}}
+    {{-- <script rel="preload" as="script" src="{{ asset('js/client/app.min.js' . Config::get('app.version')) }}"></script> --}}
   
 
   
@@ -126,9 +126,9 @@
         </div>
         @include('client.partial.footer')
     </div>
-    <script src="{{ asset('js/library/jquery.min.js' . Config::get('app.version')) }}" async></script>
-    <script src="{{ asset('js/library/tipped.min.js' . Config::get('app.version')) }}" async></script>
-    <script src="{{ asset('js/client/app.min.js' . Config::get('app.version')) }}" async></script>
+    <script defer src="{{ asset('js/library/jquery.min.js' . Config::get('app.version')) }}"></script>
+    <script defer src="{{ asset('js/library/tipped.min.js' . Config::get('app.version')) }}"></script>
+    <script defer src="{{ asset('js/client/app.min.js' . Config::get('app.version')) }}"></script>
 
     
     @yield('javascripts')
