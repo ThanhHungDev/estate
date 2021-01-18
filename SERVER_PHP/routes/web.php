1e2a002734068cdd6dd2be475ad73783dce26cd9
Trương Thanh hùng <thuong_thanh_hung@managerment-partners.co.jp>
2021-01-18 15:20:03 +0900

    update code

--- SERVER_PHP/routes/web.php
+++ SERVER_PHP/routes/web.php
@@ -13,9 +13,9 @@ use Jenssegers\Agent\Agent;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('resizes/{size}/{type}/{imagePath}', 'ImageController@resize')
-->where('imagePath', '(.*)')
-->name('IMAGE_RESIZE');
+// Route::get('resizes/{size}/{type}/{imagePath}', 'ImageController@resize')
+// ->where('imagePath', '(.*)')
+// ->name('IMAGE_RESIZE');
 
 Route::group([ 'middleware' => []], function () {
 
