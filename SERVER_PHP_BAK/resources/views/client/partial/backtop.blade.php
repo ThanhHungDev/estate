1561c64d2fd14119299071eada2b83d358697c59
T.T.Hùng <thanhhung.code@gmail.com>
2021-07-01 09:37:18 +0900

    remove

--- SERVER_PHP_BAK/resources/views/client/partial/backtop.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="backtop" onclick="backToTop()">
-    <div title="Bấm để trượt lên đầu trang" id="back-top" class="dpl_way">
-        <i class="hero-icon hero-arrow-up-bold-box-outline"></i>
-    </div>
-</div>
