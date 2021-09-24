d1beb1a8bcb3e4879cebff27a02b47d29703d866
T.T.Hùng <thanhhung.code@gmail.com>
2021-09-24 15:23:48 +0900

    rolback client router

--- SERVER_PHP/routes/routing/client.php
+++ SERVER_PHP/routes/routing/client.php
@@ -11,10 +11,12 @@ Route::group(['prefix' => '/','middleware' => [ 'HTML_MINIFIER']], function () {
     Route::get('/',[ App\Http\Controllers\ClientController::class, 'index' ])->name('HOME_PAGE');
 
     Route::get('/favourites', [ App\Http\Controllers\ClientController::class, 'favourites' ])->name('FAVOURITES');
+    Route::get('/tin-tuc', [ App\Http\Controllers\ClientController::class, 'news' ])->name('NEWS_PAGE'); 
 
 
     Route::get('/contact',[ App\Http\Controllers\ClientController::class, 'contact' ])->name('CONTACT_PAGE');
     Route::post('/contact',[ App\Http\Controllers\ClientController::class, 'mailContact' ])->name('MAIL_CONTACT');
+    Route::post('/contact-product',[ App\Http\Controllers\ClientController::class, 'mailContactProduct' ])->name('MAIL_CONTACT_PRODUCT');
 
     Route::get('/search', [ App\Http\Controllers\ClientController::class, 'search' ])->name('SEARCH');
 
@@ -51,12 +53,20 @@ Route::group(['prefix' => '/','middleware' => [ 'HTML_MINIFIER']], function () {
         Route::get('/ajax-demo', [ App\Http\Controllers\UserController::class, 'getUserInfo' ])->name('USER_AJAX');
     });
 
-    Route::get('/article/{slug?}',[ App\Http\Controllers\ClientController::class, 'viewPostArticle' ])->name('VIEW_POST_ARTICLE');
-    Route::post('/article/{slug}',[ App\Http\Controllers\ClientController::class, 'storePostArticle' ])->name('STORE_POST_ARTICLE');
+    // Route::get('/article/{slug?}',[ App\Http\Controllers\ClientController::class, 'viewPostArticle' ])->name('VIEW_POST_ARTICLE');
+    // Route::post('/article/{slug}',[ App\Http\Controllers\ClientController::class, 'storePostArticle' ])->name('STORE_POST_ARTICLE');
 
-    Route::get('/news/{slug?}',[ App\Http\Controllers\ClientController::class, 'viewNews' ])->name('VIEW_POST_NEWS');
-    Route::get('/news/{slug}',[ App\Http\Controllers\ClientController::class, 'postNews' ])->name('STORE_POST_NEWS');
+    // Route::get('/news/{slug?}',[ App\Http\Controllers\ClientController::class, 'viewNews' ])->name('VIEW_POST_NEWS');
+    // Route::get('/news/{slug}',[ App\Http\Controllers\ClientController::class, 'postNews' ])->name('STORE_POST_NEWS');
 
-    Route::get('/{slug}',[ App\Http\Controllers\ClientController::class, 'postDetail' ])->name('POST_VIEW');
+    // Route::get('/{slug}',[ App\Http\Controllers\ClientController::class, 'postDetail' ])->name('POST_VIEW');
+    Route::get('/tin-tuc/{slug}',[ App\Http\Controllers\ClientController::class, 'postDetail' ])->name('POST_VIEW');
+
+    Route::get('/tim-mua-bao-loc/{slug}',[ App\Http\Controllers\ClientController::class, 'categoryDetail' ])->name('CATEGORY_VIEW');
+    Route::get('/chu-de-bai-viet-nha-dat/{slug}',[ App\Http\Controllers\ClientController::class, 'topicDetail' ])->name('TOPIC_VIEW');
+    Route::get('/tim-mua/{slug}',[ App\Http\Controllers\ClientController::class, 'ptagDetail' ])->name('PTAG_VIEW_SEARCH');
+    Route::get('/tim-kiem',[ App\Http\Controllers\ClientController::class, 'searchProduct' ])->name('PRODUCT_SEARCH');
+    Route::get('/tim-kiem-bai-viet',[ App\Http\Controllers\ClientController::class, 'searchPost' ])->name('POST_SEARCH');
+    Route::get('/{slug}',[ App\Http\Controllers\ClientController::class, 'productDetail' ])->name('PRODUCT_VIEW');
 });
 
