27d31b3c95577357f370b9a4c7b58be90549c8dc
T.T.Hùng <thanhhung.code@gmail.com>
2021-10-05 16:37:31 +0900

    updaye code edit product

--- SERVER_PHP/resources/views/client/product-detail.blade.php
+++ SERVER_PHP/resources/views/client/product-detail.blade.php
@@ -104,12 +104,7 @@
    
 
     <div class="content__product product bg__product--detail" itemscope itemtype="https://schema.org/Article">
-        <div class="d-none" itemtype="https://schema.org/AggregateRating" itemscope>
-            <meta itemprop="reviewCount" content="{{ SupportString::createRateValueByDate($product->id) }}" />
-            <meta itemprop="ratingValue" content="{{ $product->rate_value }}" />
-            <meta itemprop="bestRating" content="5" />
-            <meta itemprop="worstRating" content="1" />
-        </div>
+
         
         <div class="d-none" itemprop="review" itemtype="https://schema.org/Review" itemscope>
             <meta itemprop="datePublished" content="{{ Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $product->created_at)->format('Y-m-d') }}" />
