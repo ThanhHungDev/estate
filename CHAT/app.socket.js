d8618ec2866fb29dcfc70f27ebb49ced33712944
hùngtt <thanhhung.dev@gmail.com>
2021-10-16 19:46:16 +0900

    update

--- CHAT/app.socket.js
+++ CHAT/app.socket.js
@@ -1,80 +1,69 @@
 ///////////////////////////////////////////////////////////////
 // define variable global user online /////////////////////////
 ///////////////////////////////////////////////////////////////
-var USER_ONLINES
+var USER_ONLINES = []
 ///////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////
 
 const CONFIG = require("./config")
-// const io = require( "socket.io" )();
+const io = require( "socket.io" )();
 const authMiddleware = require("./middlewares/jwt.middleware")
-// const eventIO = {
-//     io: io
-// };
-function socketConnecting(io){
 
-    // Add your socket.io logic here!
-    io
-    .use(async (socket, next) => {
-        console.log('middleware running...')
+// Add your socket.io logic here!
+io
+.use(async (socket, next) => {
+    console.log('middleware running...')
 
-        const { query } = socket.handshake
-        const { token } = query
-        if (!token){
-            console.log("have error")
-            // either handle it
-            // socket.disconnect(true)
-            let err  = {
-                code   : 401,
-                type   : 'token',
-                message: 'Authentication error'
-            }
-            const error = new Error(err.message)
-            error.data = err // additional detail
-            return next(error)
+    const { query } = socket.handshake
+    const { token } = query
+    if (!token){
+        console.log("have error")
+        // either handle it
+        // socket.disconnect(true)
+        let err  = {
+            code   : 401,
+            type   : 'token',
+            message: 'Authentication error'
         }
-        const auth = await authMiddleware.isAuthSocket(token)
-        if( !auth ){
-            
-            let err  = {
-                code   : 403,
-                type   : 'authentication_error',
-                message: 'Authentication error'
-            }
-            const error = new Error(err.message)
-            error.data = err // additional detail
-            return next(error)
+        const error = new Error(err.message)
+        error.data = err // additional detail
+        return next(error)
+    }
+    const auth = await authMiddleware.isAuthSocket(token)
+    if( !auth ){
+        
+        let err  = {
+            code   : 403,
+            type   : 'authentication_error',
+            message: 'Authentication error'
         }
-        socket.jwt = auth
-        next()
-    })
-    .on( CONFIG.EVENT.CONNECTION, socket => {
+        const error = new Error(err.message)
+        error.data = err // additional detail
+        return next(error)
+    }
+    socket.jwt = auth
+    next()
+})
+.on( CONFIG.EVENT.CONNECTION, socket => {
 
-        console.log("have connect: " + socket.id )
-        /// set user online active false
-        const { jwt } = socket
-        USER_ONLINES.push({ id: jwt.id, email: jwt.email, socketId : socket.id, active: false })
+    console.log("have connect: " + socket.id )
+    /// set user online active false
+    const { jwt } = socket
+    USER_ONLINES.push({ id: jwt.id, email: jwt.email, socketId : socket.id, active: false })
+    io.USER_ONLINES = USER_ONLINES
+        
+    socket.on( CONFIG.EVENT.DISCONNECT, async function () {
+
+        console.log( "disconnect set user offline")
+        USER_ONLINES = USER_ONLINES.filter(item => item.socketId != socket.id )
         io.USER_ONLINES = USER_ONLINES
-            
-        socket.on( CONFIG.EVENT.DISCONNECT, async function () {
-
-            console.log( "disconnect set user offline")
-            USER_ONLINES = USER_ONLINES.filter(item => item.socketId != socket.id )
-            io.USER_ONLINES = USER_ONLINES
-            console.log( "disconnect set user offline", USER_ONLINES)
-            socket.leaveAll()
-        })
-        socket.on( CONFIG.EVENT.JOIN__COMMENT, async data => {
-            // USER_ONLINES.map( user => {}user.socketId == socket.id ? { ...})
-        })
+        socket.leaveAll()
     })
-}
+    socket.on( CONFIG.EVENT.JOIN__COMMENT, async data => {
+        // USER_ONLINES.map( user => {}user.socketId == socket.id ? { ...})
+    })
+})
 // end of socket.io logic
 
-// module.exports = eventIO;
-
-module.exports = function(io, _online ) {
-    USER_ONLINES = _online
-    socketConnecting(io)
-};
+module.exports = io;
