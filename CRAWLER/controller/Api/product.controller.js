23a15b020c6cfdc5cb2cc6eacd53cd9e30d4e76f
hùngtt <thanhhung.dev@gmail.com>
2022-01-09 10:36:10 +0700

    setup thanh cong crawler nodejs

--- CRAWLER/controller/Api/product.controller.js
+++ CRAWLER/controller/Api/product.controller.js
@@ -1,6 +1,6 @@
-const fs        = require('fs'),
-webdriver = require('selenium-webdriver'),
-createError = require('http-errors')
+const fs           = require('fs'),
+      webdriver    = require('selenium-webdriver'),
+      createError  = require('http-errors');
 
 
 
@@ -8,31 +8,18 @@ const Product       = require("../../models/product.model")
 const responseLibrary = require("../../helpers/response.library")
 
 
-
-
 const until = webdriver.until;
 const By = webdriver.By;
-try {
-	const driver = new webdriver.Builder()
-			.forBrowser('chrome')//'phantomjs' 
-			.build();
-	// var sizeWidth = 1400;
-	// var sizeHeight = 480;
-	// resize window
-	// driver.manage().window().setSize(sizeWidth, sizeHeight);
-} catch (e) {
-	console.log("lỗi selenium" + e);
-	driver = false
-}
-
-
 
 
 
 module.exports.getProduct = async (req, res) => {
 
     const { id } = req.params
-    if( !driver ) throw createError(responseLibrary.HTTP_NOT_FOUND, "khong tìm thấy channel")
+    const driver = new webdriver.Builder()
+                    .forBrowser('chrome')
+                    .build();
+    if( !driver ) throw createError(responseLibrary.HTTP_NOT_FOUND, "khong tìm thấy chrome driver")
     await driver.get('https://www.click49.net/forum/'); // http://land.vn
 		
     var btnLogin = await driver.findElement(By.xpath("//*[contains(@class, 'p-navgroup-link--logIn')]"));
@@ -49,34 +36,12 @@ module.exports.getProduct = async (req, res) => {
     await inputPassword.sendKeys("phamdohuy150899");
 
     var btnSubmitLogin = await driver.findElement(By.xpath("//*[contains(@class, 'button--icon--login')]"));
-    await btnSubmitLogin.click();
-
-
-    let linkAvatar = await driver.wait(until.elementLocated(By.xpath("//a[contains(@class, 'p-navgroup-link--user')]")))
-    linkAvatar = await driver.wait(until.elementIsVisible(linkAvatar))
     
-
-    var testFolder = './1.png';
-    res.writeHead(200,{'Content-Type': "application/json; charset=utf-8"});
-    res.write(testFolder);
-    res.end();
-    driver.takeScreenshot().then(
-        function(image, err) {
-            fs.writeFile(testFolder, image ,  'base64' , function(err) {
-                if(err) {
-                    return console.log(err);
-                }
-                console.log("The file was saved!")
-                return;
-            }); 
-        }
-    );
-
-    // const response = {
-    //     code   : responseLibrary.HTTP_OK,
-    //     data: id,
-    //     message: "test api crawler",
-    // }
-    // return res.status(response.code).json(response)    
+    const response = {
+        code   : responseLibrary.HTTP_OK,
+        data: id,
+        message: "test api crawler",
+    }
+    return res.status(response.code).json(response)    
 }
 
